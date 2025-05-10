4c03da42b72962f58297d56fce400c23268127a9
Samaksh Gulati <65465058+samaksh97@users.noreply.github.com>
2025-05-10 12:10:01 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,6 @@
-Placeholder for index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './App.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
